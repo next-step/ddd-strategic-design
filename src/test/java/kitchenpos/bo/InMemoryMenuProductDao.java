93fc869d68ddae84e9c0d413ee4546444efccaf0
liquidjoo <liquidjoo@gmail.com>
2020-03-17 01:11:35 +0900

    test(menu) : 메뉴 테스트 코드 패키지 이동

--- src/test/java/kitchenpos/bo/InMemoryMenuProductDao.java
+++ src/test/java/kitchenpos/bo/InMemoryMenuProductDao.java
@@ -1,7 +1,7 @@
 package kitchenpos.bo;
 
-import kitchenpos.dao.MenuProductDao;
-import kitchenpos.model.MenuProduct;
+import kitchenpos.menu.domain.MenuProductDao;
+import kitchenpos.menu.domain.MenuProduct;
 
 import java.util.*;
 import java.util.stream.Collectors;
