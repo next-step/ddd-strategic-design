b59de72a08e90792adfa5fa4326d9d20be0146cf
wotjd243 <wotjd243@naver.com>
2021-08-13 14:30:09 +0900

    refactor(kitchenpos): migrate from JdbcTemplate to JPA

--- src/test/java/kitchenpos/Fixtures.java
+++ src/test/java/kitchenpos/Fixtures.java
@@ -1,127 +1,123 @@
 package kitchenpos;
 
-import kitchenpos.model.*;
+import kitchenpos.domain.*;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.Arrays;
+import java.util.Random;
+import java.util.UUID;
 
 public class Fixtures {
-    public static final Long TWO_FRIED_CHICKENS_ID = 1L;
-    public static final Long TWO_CHICKENS_ID = 1L;
-    public static final Long FRIED_CHICKEN_ID = 1L;
-    public static final Long SEASONED_CHICKEN_ID = 2L;
-    public static final Long ORDER_FOR_TABLE1_ID = 1L;
-    public static final Long TABLE1_ID = 1L;
-    public static final Long TABLE2_ID = 2L;
-    public static final Long TABLE1_AND_TABLE2_ID = 1L;
+    public static final UUID INVALID_ID = new UUID(0L, 0L);
 
-    public static Menu twoFriedChickens() {
+    public static Menu menu() {
+        return menu(19_000L, true, menuProduct());
+    }
+
+    public static Menu menu(final long price, final MenuProduct... menuProducts) {
+        return menu(price, false, menuProducts);
+    }
+
+    public static Menu menu(final long price, final boolean displayed, final MenuProduct... menuProducts) {
         final Menu menu = new Menu();
-        menu.setId(TWO_FRIED_CHICKENS_ID);
+        menu.setId(UUID.randomUUID());
         menu.setName("후라이드+후라이드");
-        menu.setPrice(BigDecimal.valueOf(19_000L));
-        menu.setMenuGroupId(TWO_CHICKENS_ID);
-        menu.setMenuProducts(Arrays.asList(menuProduct()));
+        menu.setPrice(BigDecimal.valueOf(price));
+        menu.setMenuGroup(menuGroup());
+        menu.setDisplayed(displayed);
+        menu.setMenuProducts(Arrays.asList(menuProducts));
         return menu;
     }
 
-    public static MenuGroup twoChickens() {
+    public static MenuGroup menuGroup() {
+        return menuGroup("두마리메뉴");
+    }
+
+    public static MenuGroup menuGroup(final String name) {
         final MenuGroup menuGroup = new MenuGroup();
-        menuGroup.setId(TWO_CHICKENS_ID);
-        menuGroup.setName("두마리메뉴");
+        menuGroup.setId(UUID.randomUUID());
+        menuGroup.setName(name);
         return menuGroup;
     }
 
-    public static Product friedChicken() {
-        final Product product = new Product();
-        product.setId(FRIED_CHICKEN_ID);
-        product.setName("후라이드");
-        product.setPrice(BigDecimal.valueOf(16_000L));
-        return product;
+    public static MenuProduct menuProduct() {
+        final MenuProduct menuProduct = new MenuProduct();
+        menuProduct.setSeq(new Random().nextLong());
+        menuProduct.setProduct(product());
+        menuProduct.setQuantity(2L);
+        return menuProduct;
     }
 
-    public static Product seasonedChicken() {
-        final Product product = new Product();
-        product.setId(SEASONED_CHICKEN_ID);
-        product.setName("양념치킨");
-        product.setPrice(BigDecimal.valueOf(16_000L));
-        return product;
+    public static MenuProduct menuProduct(final Product product, final long quantity) {
+        final MenuProduct menuProduct = new MenuProduct();
+        menuProduct.setSeq(new Random().nextLong());
+        menuProduct.setProduct(product);
+        menuProduct.setQuantity(quantity);
+        return menuProduct;
     }
 
-    public static Order orderForTable1() {
+    public static Order order(final OrderStatus status, final String deliveryAddress) {
         final Order order = new Order();
-        order.setId(ORDER_FOR_TABLE1_ID);
-        order.setOrderTableId(TABLE1_ID);
-        order.setOrderStatus(OrderStatus.COOKING.name());
-        order.setOrderedTime(LocalDateTime.of(2020, 1, 1, 12, 0));
+        order.setId(UUID.randomUUID());
+        order.setType(OrderType.DELIVERY);
+        order.setStatus(status);
+        order.setOrderDateTime(LocalDateTime.of(2020, 1, 1, 12, 0));
+        order.setOrderLineItems(Arrays.asList(orderLineItem()));
+        order.setDeliveryAddress(deliveryAddress);
+        return order;
+    }
+
+    public static Order order(final OrderStatus status) {
+        final Order order = new Order();
+        order.setId(UUID.randomUUID());
+        order.setType(OrderType.TAKEOUT);
+        order.setStatus(status);
+        order.setOrderDateTime(LocalDateTime.of(2020, 1, 1, 12, 0));
         order.setOrderLineItems(Arrays.asList(orderLineItem()));
         return order;
     }
 
-    public static OrderTable table1() {
-        final OrderTable orderTable = new OrderTable();
-        orderTable.setId(TABLE1_ID);
-        orderTable.setTableGroupId(null);
-        orderTable.setNumberOfGuests(0);
-        orderTable.setEmpty(false);
-        return orderTable;
+    public static Order order(final OrderStatus status, final OrderTable orderTable) {
+        final Order order = new Order();
+        order.setId(UUID.randomUUID());
+        order.setType(OrderType.EAT_IN);
+        order.setStatus(status);
+        order.setOrderDateTime(LocalDateTime.of(2020, 1, 1, 12, 0));
+        order.setOrderLineItems(Arrays.asList(orderLineItem()));
+        order.setOrderTable(orderTable);
+        return order;
     }
 
-    public static OrderTable table2() {
-        final OrderTable orderTable = new OrderTable();
-        orderTable.setId(TABLE2_ID);
-        orderTable.setTableGroupId(null);
-        orderTable.setNumberOfGuests(0);
-        orderTable.setEmpty(false);
-        return orderTable;
-    }
-
-    public static OrderTable emptyTable1() {
-        final OrderTable orderTable = table1();
-        orderTable.setEmpty(true);
-        return orderTable;
-    }
-
-    public static OrderTable emptyTable2() {
-        final OrderTable orderTable = table2();
-        orderTable.setEmpty(true);
-        return orderTable;
-    }
-
-    public static OrderTable groupedTable1() {
-        final OrderTable orderTable = table1();
-        orderTable.setTableGroupId(TABLE1_AND_TABLE2_ID);
-        return orderTable;
-    }
-
-    public static OrderTable groupedTable2() {
-        final OrderTable orderTable = table2();
-        orderTable.setTableGroupId(TABLE1_AND_TABLE2_ID);
-        return orderTable;
-    }
-
-    public static TableGroup table1AndTable2() {
-        final TableGroup tableGroup = new TableGroup();
-        tableGroup.setId(TABLE1_AND_TABLE2_ID);
-        tableGroup.setCreatedDate(LocalDateTime.of(2020, 1, 1, 12, 0));
-        tableGroup.setOrderTables(Arrays.asList(groupedTable1(), groupedTable2()));
-        return tableGroup;
-    }
-
-    private static MenuProduct menuProduct() {
-        final MenuProduct menuProduct = new MenuProduct();
-        menuProduct.setMenuId(TWO_FRIED_CHICKENS_ID);
-        menuProduct.setProductId(FRIED_CHICKEN_ID);
-        menuProduct.setQuantity(2);
-        return menuProduct;
-    }
-
-    private static OrderLineItem orderLineItem() {
+    public static OrderLineItem orderLineItem() {
         final OrderLineItem orderLineItem = new OrderLineItem();
-        orderLineItem.setOrderId(ORDER_FOR_TABLE1_ID);
-        orderLineItem.setMenuId(TWO_FRIED_CHICKENS_ID);
-        orderLineItem.setQuantity(2);
+        orderLineItem.setSeq(new Random().nextLong());
+        orderLineItem.setMenu(menu());
         return orderLineItem;
+    }
+
+    public static OrderTable orderTable() {
+        return orderTable(true, 0);
+    }
+
+    public static OrderTable orderTable(final boolean empty, final int numberOfGuests) {
+        final OrderTable orderTable = new OrderTable();
+        orderTable.setId(UUID.randomUUID());
+        orderTable.setName("1번");
+        orderTable.setNumberOfGuests(numberOfGuests);
+        orderTable.setEmpty(empty);
+        return orderTable;
+    }
+
+    public static Product product() {
+        return product("후라이드", 16_000L);
+    }
+
+    public static Product product(final String name, final long price) {
+        final Product product = new Product();
+        product.setId(UUID.randomUUID());
+        product.setName(name);
+        product.setPrice(BigDecimal.valueOf(price));
+        return product;
     }
 }
