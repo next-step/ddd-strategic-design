de0ca20942509d3836175df96a9724be864455d1
skid901 <tlrudejr1993@gmail.com>
2021-09-03 21:58:03 +0900

    refactor(kitchenpos): move jpa repositories from domain layer to infra layer

--- src/main/java/kitchenpos/menu/infra/JpaMenuGroupRepository.java
+++ src/main/java/kitchenpos/menu/infra/JpaMenuGroupRepository.java
@@ -1,5 +1,7 @@
-package kitchenpos.menu.domain;
+package kitchenpos.menu.infra;
 
+import kitchenpos.menu.domain.MenuGroup;
+import kitchenpos.menu.domain.MenuGroupRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
