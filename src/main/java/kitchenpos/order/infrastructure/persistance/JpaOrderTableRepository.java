7a79f25c1dee77e1e2fad130f24dc578679d7eb3
seok2 <seogi89@gmail.com>
2022-03-29 21:03:55 +0900

    <chore> 기능우선 패키지로 변경

---
+++ src/main/java/kitchenpos/order/infrastructure/persistance/JpaOrderTableRepository.java
@@ -0,0 +1,10 @@
+package kitchenpos.order.infrastructure.persistance;
+
+import kitchenpos.order.domain.model.OrderTable;
+import kitchenpos.order.domain.repository.OrderTableRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface JpaOrderTableRepository extends OrderTableRepository, JpaRepository<OrderTable, UUID> {
+}
