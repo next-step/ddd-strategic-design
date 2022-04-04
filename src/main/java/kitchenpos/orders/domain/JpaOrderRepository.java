0669fa673f5e905ab2946bb60993e3861386399f
정다운 <83936897+mynameisdaun@users.noreply.github.com>
2022-04-04 16:07:53 +0900

    [Step3 모델링] BoundedContext 위주의 패키지 구조로 변경 3차 PR  (#145)
    * docs(README.md) 모델링
    
    * docs(README.md) 모델링 v2
    
    * docs(README.md) 상품가격정책 용어사전, 모델링 추가
    
    * refactor (all) BoundedContext 중심의 패키지 구조로 변경
    
    * refactor (all) BoundedContext 중심의 패키지 구조로 변경 v2
    
    * refactor (all) optimize import, line formatting
    
    * refactor (all) 패키지 구조 변경

---
+++ src/main/java/kitchenpos/orders/domain/JpaOrderRepository.java
@@ -0,0 +1,9 @@
+package kitchenpos.orders.domain;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface JpaOrderRepository extends OrderRepository, JpaRepository<Order, UUID> {
+}
