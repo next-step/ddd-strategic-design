dce7a4b75e3d2de18bcaee20219a9cf2ebfde061
hiro <kiryuinh32@gmail.com>
2022-04-12 21:32:36 +0900

    Change package structure

---
+++ src/main/java/kitchenpos/order/infrastructure/DefaultKitchenridersClient.java
@@ -0,0 +1,13 @@
+package kitchenpos.order.infrastructure;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+import kitchenpos.order.domain.KitchenridersClient;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DefaultKitchenridersClient implements KitchenridersClient {
+    @Override
+    public void requestDelivery(final UUID orderId, final BigDecimal amount, final String deliveryAddress) {
+    }
+}
