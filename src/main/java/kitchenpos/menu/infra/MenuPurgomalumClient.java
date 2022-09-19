680566f5da3f4ffe0720ec7bd308423a9409e6a7
wlroh <wlroh.paul@gmail.com>
2022-09-20 00:38:45 +0900

    refactor(Menu): Menu Bounded Context 기준으로 패키지를 구분하라

--- src/main/java/kitchenpos/menu/infra/MenuPurgomalumClient.java
+++ src/main/java/kitchenpos/menu/infra/MenuPurgomalumClient.java
@@ -1,4 +1,4 @@
-package kitchenpos.infra;
+package kitchenpos.menu.infra;
 
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.stereotype.Component;
@@ -8,10 +8,10 @@ import org.springframework.web.util.UriComponentsBuilder;
 import java.net.URI;
 
 @Component
-public class DefaultPurgomalumClient implements PurgomalumClient {
+public class MenuPurgomalumClient implements PurgomalumClient {
     private final RestTemplate restTemplate;
 
-    public DefaultPurgomalumClient(final RestTemplateBuilder restTemplateBuilder) {
+    public MenuPurgomalumClient(final RestTemplateBuilder restTemplateBuilder) {
         this.restTemplate = restTemplateBuilder.build();
     }
 
--- src/main/java/kitchenpos/product/application/ProductService.java
+++ src/main/java/kitchenpos/product/application/ProductService.java
@@ -1,6 +1,8 @@
 package kitchenpos.product.application;
 
-import kitchenpos.domain.*;
+import kitchenpos.menu.domain.Menu;
+import kitchenpos.menu.domain.MenuProduct;
+import kitchenpos.menu.domain.MenuRepository;
 import kitchenpos.product.domain.Product;
 import kitchenpos.product.domain.ProductRepository;
 import kitchenpos.product.infra.PurgomalumClient;
