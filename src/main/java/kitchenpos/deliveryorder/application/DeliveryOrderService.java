6b84d5ff397c735e759cd586f0a23203fd8088d3
wlroh <wlroh.paul@gmail.com>
2022-09-21 23:24:44 +0900

    refactor(DeliveryOrder): 배달주문 컨텍스트에서 필요한 행위를 추가하라

--- src/main/java/kitchenpos/deliveryorder/application/DeliveryOrderService.java
+++ src/main/java/kitchenpos/deliveryorder/application/DeliveryOrderService.java
@@ -1,4 +1,16 @@
 package kitchenpos.deliveryorder.application;
 
+import kitchenpos.deliveryorder.domain.DeliveryOrder;
+
+import java.util.UUID;
+
 public class DeliveryOrderService {
+
+    public DeliveryOrder startDelivery(final UUID orderId) {
+        return new DeliveryOrder();
+    }
+
+    public DeliveryOrder completeDelivery(final UUID orderId) {
+        return new DeliveryOrder();
+    }
 }
