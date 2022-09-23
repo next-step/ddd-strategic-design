44e08f4f715c8d05c1d04730af51d2810d40147a
Gyeom Moon <gmoon0929@gmail.com>
2022-09-23 19:23:14 +0900

    3단계 - 기능 우선 패키지 구성하기 (#214)
    * style: 바운디드 컨텍스트 별 패키지 분리
    
    - global: 지원 서브 도메인, 애플리케이션 설정 클래스 및 공통 영역
    - menu: 메뉴 컨텍스트
    - order: 주문 컨텍스트
    - product: 상품 컨텍스트
    
    * style: Product 응용계층 패키지 추가

---
+++ src/main/java/kitchenpos/order/infra/JpaOrderRepository.java
@@ -0,0 +1,9 @@
+package kitchenpos.order.infra;
+
+import java.util.UUID;
+import kitchenpos.order.domain.Order;
+import kitchenpos.order.domain.OrderRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface JpaOrderRepository extends OrderRepository, JpaRepository<Order, UUID> {
+}
