2d14cc64ad6cd865696681f8b44d400198e965aa
Hyeon9mak <jinha3507@gmail.com>
2022-09-24 15:12:05 +0900

    refactor: bounded context 에 따른 패키지 구조 변경

---
+++ src/main/java/kitchenpos/eatinorder/domain/JpaOrderRepository.java
@@ -0,0 +1,7 @@
+package kitchenpos.eatinorder.domain;
+
+import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface JpaOrderRepository extends OrderRepository, JpaRepository<Order, UUID> {
+}
