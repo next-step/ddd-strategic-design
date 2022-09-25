039ea8daa67ab9dac7c29bf39513de3f1a28f774
Choi Seon Kang <42162127+zzangoobrother@users.noreply.github.com>
2022-09-25 22:01:02 +0900

    Step3 - 기능 우선 패키지 구성하기 (#216)
    * feat : 용어사전 정의
    
    * fix : 피드백 수정
    
    * fix : 리뷰 피드백 수정
    
    * feat : 모델링 작성
    
    * fix : 리뷰 피드백 수정
    
    * refactor : 바운디드 컨텍스트 패키지 분리
    
    * refactor : 리뷰 피드백 수정

---
+++ src/main/java/kitchenpos/order/domain/repository/OrderRepository.java
@@ -0,0 +1,19 @@
+package kitchenpos.order.domain.repository;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+import kitchenpos.order.domain.model.Order;
+import kitchenpos.order.domain.model.OrderStatus;
+import kitchenpos.order.domain.model.OrderTable;
+
+public interface OrderRepository {
+    Order save(Order order);
+
+    Optional<Order> findById(UUID id);
+
+    List<Order> findAll();
+
+    boolean existsByOrderTableAndStatusNot(OrderTable orderTable, OrderStatus status);
+}
+
