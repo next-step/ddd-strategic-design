0ca34ddd5e5b0d0bc33a9b408e6ce8ae3a163726
wlroh <wlroh.paul@gmail.com>
2022-09-27 00:23:11 +0900

    feat(TakeoutOrderService): 포장 지문 기능을 구현하라

--- src/main/java/kitchenpos/takeoutorder/application/TakeoutOrderService.java
+++ src/main/java/kitchenpos/takeoutorder/application/TakeoutOrderService.java
@@ -1,4 +1,30 @@
 package kitchenpos.takeoutorder.application;
 
+import kitchenpos.eatinorder.order.domain.EatInOrder;
+import kitchenpos.takeoutorder.domain.TakeoutOrder;
+
+import java.util.List;
+import java.util.UUID;
+
 public class TakeoutOrderService {
+
+    public TakeoutOrder create(final EatInOrder request) {
+        return new TakeoutOrder();
+    }
+
+    public TakeoutOrder accept(final UUID orderId) {
+        return new TakeoutOrder();
+    }
+
+    public TakeoutOrder serve(final UUID orderId) {
+        return new TakeoutOrder();
+    }
+
+    public TakeoutOrder complete(final UUID orderId) {
+        return new TakeoutOrder();
+    }
+
+    public List<TakeoutOrder> findAll() {
+        return List.of(new TakeoutOrder());
+    }
 }
