54ca2b11cd82a0766c8779890566e455cdde15f0
wlroh <wlroh.paul@gmail.com>
2022-09-27 00:22:48 +0900

    feat(DeliveryOrderService): 배달 주문 기능을 구현하라

--- src/main/java/kitchenpos/deliveryorder/application/DeliveryOrderService.java
+++ src/main/java/kitchenpos/deliveryorder/application/DeliveryOrderService.java
@@ -1,10 +1,24 @@
 package kitchenpos.deliveryorder.application;
 
 import kitchenpos.deliveryorder.domain.DeliveryOrder;
+import kitchenpos.eatinorder.order.domain.EatInOrder;
 
+import java.util.List;
 import java.util.UUID;
 
 public class DeliveryOrderService {
+
+    public DeliveryOrder create(final EatInOrder request) {
+        return new DeliveryOrder();
+    }
+
+    public DeliveryOrder accept(final UUID orderId) {
+        return new DeliveryOrder();
+    }
+
+    public DeliveryOrder serve(final UUID orderId) {
+        return new DeliveryOrder();
+    }
 
     public DeliveryOrder startDelivery(final UUID orderId) {
         return new DeliveryOrder();
@@ -12,5 +26,13 @@ public class DeliveryOrderService {
 
     public DeliveryOrder completeDelivery(final UUID orderId) {
         return new DeliveryOrder();
+    }
+
+    public DeliveryOrder complete(final UUID orderId) {
+        return new DeliveryOrder();
+    }
+
+    public List<DeliveryOrder> findAll() {
+        return List.of(new DeliveryOrder());
     }
 }
