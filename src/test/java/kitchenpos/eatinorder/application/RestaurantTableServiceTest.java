db159832edc1329e11a65aabd3415bfc44545793
hiblue02 <hiblue02@gmail.com>
2023-09-05 00:28:31 +0900

    refactor(eatInOrder): 매장 테이블 테스트 코드 수정 - orderTable > RestaurantTable 이름 변경

--- src/test/java/kitchenpos/eatinorder/application/RestaurantTableServiceTest.java
+++ src/test/java/kitchenpos/eatinorder/application/RestaurantTableServiceTest.java
@@ -1,11 +1,14 @@
 package kitchenpos.eatinorder.application;
 
 import kitchenpos.Fixtures;
-import kitchenpos.application.InMemoryOrderRepository;
-import kitchenpos.application.InMemoryOrderTableRepository;
-import kitchenpos.domain.OrderRepository;
 import kitchenpos.domain.OrderStatus;
+import kitchenpos.eatinorder.domain.InMemoryEatInOrderRepository;
+import kitchenpos.eatinorder.domain.InMemoryRestaurantTableRepository;
 import kitchenpos.order.eatinorder.application.RestaurantTableService;
+import kitchenpos.order.eatinorder.domain.EatInOrderRepository;
+import kitchenpos.order.eatinorder.domain.EatInOrderStatus;
+import kitchenpos.order.eatinorder.domain.RestaurantTable;
+import kitchenpos.order.eatinorder.domain.RestaurantTableRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -23,22 +26,22 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
 class RestaurantTableServiceTest {
-    private OrderTableRepository orderTableRepository;
-    private OrderRepository orderRepository;
+    private RestaurantTableRepository restaurantTableRepository;
+    private EatInOrderRepository eatInOrderRepository;
     private RestaurantTableService restaurantTableService;
 
     @BeforeEach
     void setUp() {
-        orderTableRepository = new InMemoryOrderTableRepository();
-        orderRepository = new InMemoryOrderRepository();
-        restaurantTableService = new RestaurantTableService(orderTableRepository, orderRepository);
+        restaurantTableRepository = new InMemoryRestaurantTableRepository();
+        eatInOrderRepository = new InMemoryEatInOrderRepository();
+        restaurantTableService = new RestaurantTableService(restaurantTableRepository, eatInOrderRepository);
     }
 
     @DisplayName("주문 테이블을 등록할 수 있다.")
     @Test
     void create() {
-        final OrderTable expected = createOrderTableRequest("1번");
-        final OrderTable actual = restaurantTableService.create(expected);
+        final RestaurantTable expected = createRestaurantTableRequest("1번");
+        final RestaurantTable actual = restaurantTableService.create(expected);
         assertThat(actual).isNotNull();
         assertAll(
             () -> assertThat(actual.getId()).isNotNull(),
@@ -52,7 +55,7 @@ class RestaurantTableServiceTest {
     @NullAndEmptySource
     @ParameterizedTest
     void create(final String name) {
-        final OrderTable expected = createOrderTableRequest(name);
+        final RestaurantTable expected = createRestaurantTableRequest(name);
         assertThatThrownBy(() -> restaurantTableService.create(expected))
             .isInstanceOf(IllegalArgumentException.class);
     }
@@ -60,16 +63,16 @@ class RestaurantTableServiceTest {
     @DisplayName("빈 테이블을 해지할 수 있다.")
     @Test
     void sit() {
-        final UUID orderTableId = orderTableRepository.save(Fixtures.restaurantTable(false, 0)).getId();
-        final OrderTable actual = restaurantTableService.sit(orderTableId);
+        final UUID orderTableId = restaurantTableRepository.save(Fixtures.restaurantTable(false, 0)).getId();
+        final RestaurantTable actual = restaurantTableService.sit(orderTableId);
         assertThat(actual.isOccupied()).isTrue();
     }
 
     @DisplayName("빈 테이블로 설정할 수 있다.")
     @Test
     void clear() {
-        final UUID orderTableId = orderTableRepository.save(Fixtures.restaurantTable(true, 4)).getId();
-        final OrderTable actual = restaurantTableService.clear(orderTableId);
+        final UUID orderTableId = restaurantTableRepository.save(Fixtures.restaurantTable(true, 4)).getId();
+        final RestaurantTable actual = restaurantTableService.clear(orderTableId);
         assertAll(
             () -> assertThat(actual.getNumberOfGuests()).isZero(),
             () -> assertThat(actual.isOccupied()).isFalse()
@@ -79,19 +82,19 @@ class RestaurantTableServiceTest {
     @DisplayName("완료되지 않은 주문이 있는 주문 테이블은 빈 테이블로 설정할 수 없다.")
     @Test
     void clearWithUncompletedOrders() {
-        final OrderTable orderTable = orderTableRepository.save(Fixtures.restaurantTable(true, 4));
-        final UUID orderTableId = orderTable.getId();
-        orderRepository.save(order(OrderStatus.ACCEPTED, orderTable));
-        assertThatThrownBy(() -> restaurantTableService.clear(orderTableId))
+        final RestaurantTable restaurantTable = restaurantTableRepository.save(Fixtures.restaurantTable(true, 4));
+        final UUID restaurantTableId = restaurantTable.getId();
+        eatInOrderRepository.save(eatInOrder(EatInOrderStatus.ACCEPTED, restaurantTable));
+        assertThatThrownBy(() -> restaurantTableService.clear(restaurantTableId))
             .isInstanceOf(IllegalStateException.class);
     }
 
     @DisplayName("방문한 손님 수를 변경할 수 있다.")
     @Test
     void changeNumberOfGuests() {
-        final UUID orderTableId = orderTableRepository.save(Fixtures.restaurantTable(true, 0)).getId();
-        final OrderTable expected = changeNumberOfGuestsRequest(4);
-        final OrderTable actual = restaurantTableService.changeNumberOfGuests(orderTableId, expected);
+        final UUID orderTableId = restaurantTableRepository.save(Fixtures.restaurantTable(true, 0)).getId();
+        final RestaurantTable expected = changeNumberOfGuestsRequest(4);
+        final RestaurantTable actual = restaurantTableService.changeNumberOfGuests(orderTableId, expected);
         assertThat(actual.getNumberOfGuests()).isEqualTo(4);
     }
 
@@ -99,8 +102,8 @@ class RestaurantTableServiceTest {
     @ValueSource(ints = -1)
     @ParameterizedTest
     void changeNumberOfGuests(final int numberOfGuests) {
-        final UUID orderTableId = orderTableRepository.save(Fixtures.restaurantTable(true, 0)).getId();
-        final OrderTable expected = changeNumberOfGuestsRequest(numberOfGuests);
+        final UUID orderTableId = restaurantTableRepository.save(Fixtures.restaurantTable(true, 0)).getId();
+        final RestaurantTable expected = changeNumberOfGuestsRequest(numberOfGuests);
         assertThatThrownBy(() -> restaurantTableService.changeNumberOfGuests(orderTableId, expected))
             .isInstanceOf(IllegalArgumentException.class);
     }
@@ -108,8 +111,8 @@ class RestaurantTableServiceTest {
     @DisplayName("빈 테이블은 방문한 손님 수를 변경할 수 없다.")
     @Test
     void changeNumberOfGuestsInEmptyTable() {
-        final UUID orderTableId = orderTableRepository.save(Fixtures.restaurantTable(false, 0)).getId();
-        final OrderTable expected = changeNumberOfGuestsRequest(4);
+        final UUID orderTableId = restaurantTableRepository.save(Fixtures.restaurantTable(false, 0)).getId();
+        final RestaurantTable expected = changeNumberOfGuestsRequest(4);
         assertThatThrownBy(() -> restaurantTableService.changeNumberOfGuests(orderTableId, expected))
             .isInstanceOf(IllegalStateException.class);
     }
@@ -117,20 +120,20 @@ class RestaurantTableServiceTest {
     @DisplayName("주문 테이블의 목록을 조회할 수 있다.")
     @Test
     void findAll() {
-        orderTableRepository.save(restaurantTable());
-        final List<OrderTable> actual = restaurantTableService.findAll();
+        restaurantTableRepository.save(restaurantTable());
+        final List<RestaurantTable> actual = restaurantTableService.findAll();
         assertThat(actual).hasSize(1);
     }
 
-    private OrderTable createOrderTableRequest(final String name) {
-        final OrderTable orderTable = new OrderTable();
-        orderTable.setName(name);
-        return orderTable;
+    private RestaurantTable createRestaurantTableRequest(final String name) {
+        final RestaurantTable restaurantTable = new RestaurantTable();
+        restaurantTable.setName(name);
+        return restaurantTable;
     }
 
-    private OrderTable changeNumberOfGuestsRequest(final int numberOfGuests) {
-        final OrderTable orderTable = new OrderTable();
-        orderTable.setNumberOfGuests(numberOfGuests);
-        return orderTable;
+    private RestaurantTable changeNumberOfGuestsRequest(final int numberOfGuests) {
+        final RestaurantTable restaurantTable = new RestaurantTable();
+        restaurantTable.setNumberOfGuests(numberOfGuests);
+        return restaurantTable;
     }
 }
