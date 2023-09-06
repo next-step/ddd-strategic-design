b236440a1d4d4b6be417d4c6db6ba51be1bb0f04
JungTag <ytaek@gmarket.com>
2023-09-07 08:14:02 +0900

    3단계 - 기능 우선 패키지 구성하기 (#286)
    * refactor: 바운디드 컨텍스트 중심의 패키지 구조로 리팩터링
    
    * refactor: 리뷰 반영

---
+++ src/main/java/kitchenpos/purgomalumclient/domain/PurgomalumClient.java
@@ -0,0 +1,5 @@
+package kitchenpos.purgomalumclient.domain;
+
+public interface PurgomalumClient {
+    boolean containsProfanity(String text);
+}
--- src/test/java/kitchenpos/application/MenuServiceTest.java
+++ src/test/java/kitchenpos/application/MenuServiceTest.java
@@ -1,7 +1,10 @@
 package kitchenpos.application;
 
-import kitchenpos.domain.*;
-import kitchenpos.infra.PurgomalumClient;
+import kitchenpos.menu.domain.*;
+import kitchenpos.menu.application.MenuService;
+import kitchenpos.product.domain.Product;
+import kitchenpos.product.domain.ProductRepository;
+import kitchenpos.purgomalumclient.domain.PurgomalumClient;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
