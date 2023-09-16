76687468549364da7d2f11ed2dea2a1f10847648
김경록 <kyeongrok.kim@kurlycorp.com>
2023-09-17 01:20:28 +0900

    refactor: 누락된 menu 컨텍스트 패키지 분리

--- src/test/java/kitchenpos/Fixtures.java
+++ src/test/java/kitchenpos/Fixtures.java
@@ -1,13 +1,12 @@
 package kitchenpos;
 
-import kitchenpos.domain.*;
-
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.Arrays;
 import java.util.Random;
 import java.util.UUID;
 import kitchenpos.menu.domain.Menu;
+import kitchenpos.menu.domain.MenuProduct;
 import kitchenpos.menugroup.domain.MenuGroup;
 import kitchenpos.order.domain.Order;
 import kitchenpos.order.domain.OrderLineItem;
