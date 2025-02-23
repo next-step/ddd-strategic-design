9be77c02c623a158f92c8c69dde3f297300a6db7
DAWN <86773987+all-cloudz@users.noreply.github.com>
2025-02-23 19:05:14 +0900

    refactor(kitchenpos): 패키지 재구성 (#495)
    * refactor(kitchenpos): 패키지 재구성
    
    * fix(kitchenpos): 잘못된 패키지 수정

---
+++ src/main/java/kitchenpos/order/common/domain/model/OrderStatus.java
@@ -0,0 +1,5 @@
+package kitchenpos.order.common.domain.model;
+
+public enum OrderStatus {
+    WAITING, ACCEPTED, SERVED, DELIVERING, DELIVERED, COMPLETED
+}
---
+++ src/main/java/kitchenpos/order/eatin/domain/service/EatInOrderService.java
@@ -0,0 +1,4 @@
+package kitchenpos.order.eatin.domain.service;
+
+public class EatInOrderService {
+}
